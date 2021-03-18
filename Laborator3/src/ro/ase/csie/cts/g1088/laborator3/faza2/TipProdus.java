26ff02fc6417b5f04e1a7bdd138cc5830f5f6aca
Emilia <dumitruemiliagabriela@gmail.com>
2021-03-18 12:37:24 +0200

    commit la pauza

--- Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza2/TipProdus.java
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza2/TipProdus.java
@@ -1,4 +1,4 @@
-package ro.ase.csie.cts.g1088.laborator3.faza1.faza2;
+package ro.ase.csie.cts.g1088.laborator3.faza2;
 
 public enum TipProdus {
 	NOU(0),DISCOUNT(0.1f),STOC_LIMITAT(0.25f),VECHI(0.35f);
