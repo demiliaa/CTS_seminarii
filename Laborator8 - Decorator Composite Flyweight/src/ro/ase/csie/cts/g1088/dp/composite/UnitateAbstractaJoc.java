f11081cfd5d0209a1236029f7dd8bb5e6a0bb575
Emilia <dumitruemiliagabriela@gmail.com>
2021-04-15 13:25:54 +0300

    Composite

---
+++ Laborator8 - Decorator Composite Flyweight/src/ro/ase/csie/cts/g1088/dp/composite/UnitateAbstractaJoc.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1088.dp.composite;
+
+public abstract class UnitateAbstractaJoc implements InterfataNPC{
+//sa sugereze caractere, grupuri
+	//interfata nodurilor frunza este mostenita
+	//definim interfata specifica nodurilor copil
+	public void adaugaCaracter(UnitateAbstractaJoc unitate) {
+		throw new UnsupportedOperationException();
+	}
+	public  void stergeCaracter(UnitateAbstractaJoc unitate) {
+		throw new UnsupportedOperationException();
+	}
+	public UnitateAbstractaJoc getCaracter(int index) {
+		throw new UnsupportedOperationException();
+	}
+}
