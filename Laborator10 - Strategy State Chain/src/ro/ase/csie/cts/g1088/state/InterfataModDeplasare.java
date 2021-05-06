b3c9eee5b4ff7ba38ae94277e2fd8bd62f4d8e11
Emilia <dumitruemiliagabriela@gmail.com>
2021-05-06 13:36:27 +0300

    Implementare State si inceput de Chain

---
+++ Laborator10 - Strategy State Chain/src/ro/ase/csie/cts/g1088/state/InterfataModDeplasare.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1088.state;
+
+public interface InterfataModDeplasare {
+	public void seMisca(SuperErou erou);
+
+}
---
+++ Laborator10 - Strategy State Chain/src/ro/ase/csie/cts/g1088/state/StareDeplasareRanitCritic.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1088.state;
+
+public class StareDeplasareRanitCritic implements InterfataModDeplasare {
+
+	@Override
+	public void seMisca(SuperErou erou) {
+		System.out.println(erou.nume + " nu se poate deplasa");
+		
+	}
+	
+
+}
