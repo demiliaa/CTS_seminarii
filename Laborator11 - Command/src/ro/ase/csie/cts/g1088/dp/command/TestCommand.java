aa8039932a27ca6a7600b2478c3a1bb8afd88000
Emilia <dumitruemiliagabriela@gmail.com>
2021-05-14 16:26:04 +0300

    Command

---
+++ Laborator11 - Command/src/ro/ase/csie/cts/g1088/dp/command/TestCommand.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.cts.g1088.dp.command;
+
+public class TestCommand {
+
+	public static void main(String[] args) {
+		ManagerTaskuri manager=new ManagerTaskuri();
+		manager.adaugaTask(new TaskAsincron(new ModulUpdate(), "10.1", 5));
+		manager.adaugaTask(new TaskAsincron(new ModulUpdate(), "Final nivel 5", 1));
+		
+		System.out.println("Jocul continua...");
+		
+		manager.executaUrmatorulTask();
+		
+		System.out.println("Jocul continua");
+		
+		manager.executaUrmatorulTask();
+		
+		System.out.println("Jocul continua");
+	}
+
+}
